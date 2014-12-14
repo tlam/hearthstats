a73f8d33d69d57f5591d407a04341709b5dbccf0
Thierry <lamthierry@gmail.com>
2014-12-14 15:35:59 -0500

    Adding heroku deploy files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hearthstats.wsgi
--- hearthstats/settings.py
+++ hearthstats/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import dj_database_url
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -59,10 +62,7 @@ WSGI_APPLICATION = 'hearthstats.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Internationalization
