117388c614b288964fedcb1a18695e1d1c995c91
Thierry <lamthierry@gmail.com>
2017-04-09 16:01:59 -0400

    Display card image in a modal

--- hearthstats/static/js/hearthstats.js
+++ hearthstats/static/js/hearthstats.js
@@ -7,23 +7,35 @@ app.config(function($interpolateProvider) {
 app.controller('RarityCtrl', function($scope, $http, $attrs) {
   if ($attrs.code !== '') {
     $http.get('/cards/' + $attrs.code + '/set_rarity.json').success(function(data) {
-      for (var rarity in data.rarity) {
-        var stats = data.rarity[rarity];
-        var total = stats['total'];
-        var own = stats['own'];
-        var chartData = google.visualization.arrayToDataTable([
-          ['Card status', 'Number of cards'],
-          ['Own', own],
-          ['Missing', total - own],
-        ]);
 
-        var options = {
-          title: rarity
-        };
+      function drawChart() {
+        for (var rarity in data.rarity) {
+          var stats = data.rarity[rarity];
+          var total = stats['total'];
+          var own = stats['own'];
+          var chartData = google.visualization.arrayToDataTable([
+            ['Card status', 'Number of cards'],
+            ['Own', own],
+            ['Missing', total - own],
+          ]);
 
-        var chart = new google.visualization.PieChart(document.getElementById(rarity.toLowerCase() + '-chart'));
-        chart.draw(chartData, options);
+          var options = {
+            title: rarity
+          };
+
+          var chart = new google.visualization.PieChart(document.getElementById(rarity.toLowerCase() + '-chart'));
+          chart.draw(chartData, options);
+        }
       }
+
+      google.charts.setOnLoadCallback(drawChart);
     });
   }
+});
+
+$(document).ready(function(){
+  $('a.card').on('click', function() {
+    $('.modal-title').html('<span>' + $(this).attr('data-title') + '</span>');
+    $('.modal-body').html('<img src="' + $(this).attr('data-content') + '" />');
+  });
 });
