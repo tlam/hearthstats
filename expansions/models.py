1987894a1223027e38cc3bf19138b56a0844bb2b
Thierry <lamthierry@gmail.com>
2017-12-07 21:01:38 -0500

    Increase max length of expansion code

--- expansions/models.py
+++ expansions/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Expansion(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=50, unique=True)
     release_date = models.DateField()
 
     class Meta:
